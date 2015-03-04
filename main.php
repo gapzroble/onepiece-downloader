47e54ead5b5e40dc79a6bed60ab0e7e9e5fa8977
Arcanys | Randolph Roble <r.roble@arcanys.com>
2015-03-04 10:15:50 +0800

    Update main.php

--- main.php
+++ main.php
@@ -2,10 +2,9 @@
 
 require_once __DIR__.'/vendor/autoload.php';
 
+$url = 'http://www.mangareader.net/one-piece/%d/%d';
 $episode = isset($argv[1]) ? (int) $argv[1] : 779;
 
-$logger = new Monolog\Logger('main');
-
-$dl = new Downloader('http://www.mangareader.net/one-piece/%d/%d', __DIR__.'/output');
-$dl->setLogger($logger)
+$dl = new Downloader($url, __DIR__.'/output');
+$dl->setLogger(new Monolog\Logger('main'))
         ->start($episode);
